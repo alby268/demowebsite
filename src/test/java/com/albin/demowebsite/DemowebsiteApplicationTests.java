cb7cafc480405c05c436f87032a08303dda41684
Albin Baby Peedieakkal <albinbaby26895@gmail.com>
2021-11-09 01:01:01 -0500

    spring initialization

---
+++ src/test/java/com/albin/demowebsite/DemowebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albin.demowebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemowebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
